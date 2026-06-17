4d1c148bd8d97f3ea8dc68115a2f8654a0202788
jhonnygarciazelaya <zgjhonny@gmail.com>
2026-06-17 00:20:45 -0400

    Se incorporan pruebas automatizadas para endpoints de la API

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,4 +15,41 @@ def test_health():
 
     assert response.status_code == 200
     assert "estado" in response.json()
-    assert "modelo_disponible" in response.json()
+    # assert "modelo_disponible" in response.json()
+    assert "modelo" in response.json()
+
+#pruebas para /predict 
+def test_predict_valido():
+
+    payload = {
+        "antiguedad": 24,
+        "cargo_mensual": 80.5,
+        "reclamos": 1
+    }
+
+    response = client.post(
+        "/predict",
+        json=payload
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert "prediccion" in data
+    assert "probabilidad" in data
+    assert "version_modelo" in data
+# 
+def test_predict_error_422():
+
+    payload = {
+        "antiguedad": 24,
+        "cargo_mensual": 80.5
+    }
+
+    response = client.post(
+        "/predict",
+        json=payload
+    )
+
+    assert response.status_code == 422
